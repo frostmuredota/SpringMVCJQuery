ddd706269fa29f0032badc1d96897d73bace5e94
Ramon <ramduran2011@gmail.com>
2015-09-08 18:49:26 -0300

    Validate Login finish

--- src/main/webapp/WEB-INF/views/inicio.jsp
+++ src/main/webapp/WEB-INF/views/inicio.jsp
@@ -7,8 +7,6 @@
 <link href="<c:url value="/resources/css/main.css" />" rel="stylesheet">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-<!--  <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCJnj2nWoM86eU8Bq2G4lSNz3udIkZT4YY&sensor=false"></script>-->
-<script type="text/javascript" src="<c:url value="/resources/js/javascript.js" />"></script>
 <script type="text/javascript" src="<c:url value="/resources/js/initsession.js" />"></script>
 </head>
 <body>
@@ -16,15 +14,17 @@
 	<br>
 	<div class="index">
 		<h2>${message}</h2>
-		<form method="post" action="exito.html" id="session" novalidate="novalidate" onsubmit="return initsession();">
+		<form method="post" action="exito.html" id="session" novalidate="novalidate">
 			<table align="center">
 				<tr>
-					<td>Username:</td>
-					<td><input name="username" id="username"/></td>
+					<td><label for="username">Username:</label></td>
+					<td><input name="username" id="username"/>
+					<label id="l1"></label></td>
 				</tr>
 				<tr>
-					<td>Password:</td>
-					<td><input name="password" type="password" id="password"/></td>
+					<td><label for="password">Password:</label></td>
+					<td><input name="password" type="password" id="password"/>
+					<label id="l2"></label></td>
 				</tr>
 				<tr>
 				<td colspan=2><input type="submit" name="submit" value="Log In" /></td>
@@ -32,7 +32,7 @@
 			</table>
 		</form>
 		<h3><font color="red">${error}</font></h3>
-		<a href="registro.html">For register, click here!! </a>
+		
 	</div>
 </body>
 </html>
--- src/main/webapp/resources/js/initsession.js
+++ src/main/webapp/resources/js/initsession.js
@@ -1,75 +1,79 @@
-function initsession(){
-	var a = document.getElementById("username").value.length;
-	var b = document.getElementById("password").value.length;
-	
-	if(a == 0 && b ==0){
-		alert("Please, complete all the fields");
-		return false;
-	}else{
-		if(validateUser(document.getElementById("username").value,"Please complete the field username")){
-			if(validatePassword(document.getElementById("password").value.length,6,8)){
-				return true;
+$(document).ready(function() {
+
+	failpasslength = "Please length between 4 to 30";
+	failuserlength = "Please length between 4 to 30";
+	failpassvalues = "Please use alphanumerics characters";
+	failalphanumerics = "Please use alphanumerics values";
+	failinput = "Please fill out in this field";
+
+	var username = $('#username');
+	var pass = $('#password');
+	$('#l1').css("color", "red");
+	$('#l2').css("color", "red");
+	$('input:text').click(function() {
+		//$('#l1').css("color", "red").fadeOut(500);
+		$('#l1').css("color", "red").hide();
+	});
+
+	// Change the color to inputs type password
+	$('input:password').click(function() {
+		//$('#l2').css("color", "red").fadeOut(500);
+		$('#l2').css("color", "red").hide();
+	});
+
+	$("#session").submit(function() {
+		if ((username.val() == "") && (pass.val() == "")) {
+			$("#l1").text(failinput);
+			$("#l2").text(failinput);
+			$('#l1').show();
+			$('#l2').show();
+			
+			return false;
+		} else {
+			var regx = /^([a-zA-Z0-9])+/;
+			if((username.val().length > 4) && (username.val().length < 30)){
+				if(regx.test(username.val())){
+					if((pass.val().length > 4) && (pass.val().length < 30)){
+						return true;
+					}else{
+						$("#l2").text(failpasslength);
+						$('#l2').show();
+						//$('#l1').show();
+						return false;
+					}
+				}else{
+					
+					$("#l1").text(failalphanumerics);
+					$('#l1').show();
+					$('#l2').show();
+					return false;
+				}
+				
 			}else{
+				
+				$("#l1").text(failuserlength);
+				$('#l1').show();
+				$('#l2').show();
 				return false;
 			}
-		}else{
 			return false;
 		}
-	}
-}
 
+	});
 
-var validateUser = function(element,message){
-	if(element.length!=""){
-		return true;
-	}else{
-		alert(message);
-		return false;
-	}
-};
-function validatePassword(element, min, max){
-	if(element >= min && element <= max){
-		return true;
-	}else{
-		alert("Please enter between " +min+ " and " +max+ " characters");
-		return false;
-	}
-}
+});
 
-
-
-
-/* JQUERY VALIDATE
-$(function() {
-	  
-    // Setup form validation on the #register-form element
-    $("#session").validate({
-    
-        // Specify the validation rules
-        rules: {
-            username: "required",
-            password: {
-                required: true,
-                minlength: 6
-             
-            }
-           
-        },
-        
-        // Specify the validation error messages
-        messages: {
-            username: "Please enter your username",
-            password: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 6 characters long"
-               
-            }
-        },
-        
-        submitHandler: function(form) {
-            form.submit();
-        }
-    });
-
-  });
-  	*/
+/*
+ * $(":input").focus(function(){ if ($(this).hasClass("fail") ) {
+ * $(this).val(""); $(this).removeClass("fail"); } }); /* JQUERY VALIDATE
+ * $(function() { // Setup form validation on the #register-form element
+ * $("#session").validate({ // Specify the validation rules rules: { username:
+ * "required", password: { required: true, minlength: 6 } }, // Specify the
+ * validation error messages messages: { username: "Please enter your username",
+ * password: { required: "Please provide a password", minlength: "Your password
+ * must be at least 6 characters long" } },
+ * 
+ * submitHandler: function(form) { form.submit(); } });
+ * 
+ * });
+ */
